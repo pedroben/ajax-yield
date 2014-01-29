0525b2e77a19e9154c2e7aac8cf2367d5effb536
al037665 <al037665@s09-pc08>
2014-01-29 12:17:38 +0100

    Validación del formulario de empresa con el plugin de Alvaro

--- src/main/webapp/js/control/empresa.js
+++ src/main/webapp/js/control/empresa.js
@@ -86,23 +86,148 @@ var control_empresa_list = function(path) {
             return false;
         });
 
-//        //clave ajena usuario
-        cargaClaveAjena('#id_usuario', '#id_usuario_desc', 'usuario')
-        $(prefijo_div + '#id_usuario_button').unbind('click');
-        $(prefijo_div + '#id_usuario_button').click(function() {
-            loadForeign('usuario', '#modal02', control_usuario_list, callbackSearchHilo);
-            function callbackSearchHilo(id) {
-                $(prefijo_div + '#modal02').modal('hide');
-                $(prefijo_div + '#modal02').data('modal', null);
-                $(prefijo_div + '#id_usuario').val($(this).attr('id'));
-                cargaClaveAjena('#id_usuario', '#id_usuario_desc', 'usuario');
-                return false;
+////        //clave ajena usuario
+//        cargaClaveAjena('#id_usuario', '#id_usuario_desc', 'usuario')
+//        $(prefijo_div + '#id_usuario_button').unbind('click');
+//        $(prefijo_div + '#id_usuario_button').click(function() {
+//            loadForeign('usuario', '#modal02', control_usuario_list, callbackSearchHilo);
+//            function callbackSearchHilo(id) {
+//                $(prefijo_div + '#modal02').modal('hide');
+//                $(prefijo_div + '#modal02').data('modal', null);
+//                $(prefijo_div + '#id_usuario').val($(this).attr('id'));
+//                cargaClaveAjena('#id_usuario', '#id_usuario_desc', 'usuario');
+//                return false;
+//            }
+//            return false;
+//        });
+        // Validación del formulario.
+        //http://jqueryvalidation.org/documentation/
+        $('#formulario').validate({
+            rules: {
+                id_usuario: {
+                    required: true,
+                    // maxlength: 255
+                },
+                nombre: {
+                    required: true,
+                    maxlength: 255
+                },
+                cif: {
+                    required: true,
+                    maxlength: 255
+                },
+                direccion: {
+                    required: true,
+                    maxlength: 255
+                            //       digits: true
+                },
+                localidad: {
+                    required: true,
+                    maxlength: 255
+                },
+                provincia: {
+                    required: true,
+                    maxlength: 255
+                },
+                pais: {
+                    required: true,
+                    maxlength: 255
+                },
+                telefono: {
+                    required: true,
+                    maxlength: 255
+                },
+                fax: {
+                    required: true,
+                    maxlength: 255
+                },
+                actividad: {
+                    required: true,
+                    maxlength: 255
+                },
+                nombrecontacto: {
+                    required: true,
+                    maxlength: 255
+                },
+                emailcontacto: {
+                    required: true,
+                    maxlength: 255,
+                    email: true
+                },
+                validada: {
+                    required: true,
+                    maxlength: 255
+                }
+
+            },
+            messages: {
+                id_usuario: {
+                    required: "Elige un id_usuario.",
+                },
+                nombre: {
+                    required: "Introduce un Nombre."
+                },
+                cif: {
+                    required: "Introduce un Cif.",
+                },
+                direccion: {
+                    required: "Introduce una Direccion.",
+                },
+                localidad: {
+                    required: "Introduce una Localidad",
+                    maxlength: "Tiene que ser menos de 255 caracteres"
+                },
+                provincia: {
+                    required: "Introduce una Provincia",
+                    maxlength: "Tiene que ser menos de 255 caracteres"
+                },
+                pais: {
+                    required: "Introduce un Pais",
+                    maxlength: "Tiene que ser menos de 255 caracteres"
+                },
+                telefono: {
+                    required: "Introduce un Telefono",
+                    maxlength: "Tiene que ser menos de 255 caracteres"
+                },
+                fax: {
+                    required: "Introduce un Fax",
+                    maxlength: "Tiene que ser menos de 255 caracteres"
+                },
+                actividad: {
+                    required: "Introduce una Actividad",
+                    maxlength: "Tiene que ser menos de 255 caracteres"
+                },
+                nombrecontacto: {
+                    required: "Introduce un Nombre de Contacto",
+                    maxlength: "Tiene que ser menos de 255 caracteres"
+                },
+                emailcontacto: {
+                    required: "Introduce un Correo Electrónico",
+                    maxlength: "Tiene que ser menos de 255 caracteres",
+                    email: "Introduzca un correo correcto"
+                },
+                validada: {
+                    required: "Introduzca 1 ó 0",
+                    maxlength: "Tiene que ser menos de 255 caracteres"
+                }
+
+
+            },
+            highlight: function(element) {
+                $(element).closest('.control-group').removeClass('success').addClass('error');
+            },
+            success: function(element) {
+                element
+                        .text('OK!').addClass('valid')
+                        .closest('.control-group').removeClass('error').addClass('success');
             }
-            return false;
         });
+
         $(prefijo_div + '#submitForm').unbind('click');
         $(prefijo_div + '#submitForm').click(function() {
-            enviarDatosUpdateForm(view, prefijo_div);
+            if ($('#formulario').valid()) {
+                enviarDatosUpdateForm(view, prefijo_div);
+            }
             return false;
         });
     }
