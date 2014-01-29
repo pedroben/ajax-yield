5742a81ee8b7d024c96edc37eeb7aa06b6624d42
al037684 <al037684@s09-pc15>
2014-01-29 11:41:40 +0100

    comentarioBean

---
+++ src/main/java/net/daw/bean/ComentarioBean.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author al037684
+ */
+public class ComentarioBean {
+    
+    private Integer id = 0;
+    private String titulo = "";
+    private String contenido = "";
+    private Date fecha = new Date();
+    private Integer id_usuario = 0;
+    private Integer id_comentario = 0;
+    
+    
+    
+    public ComentarioBean () {       
+    }
+    
+    public ComentarioBean(Integer intId) {
+        this.id = intId;
+    }
+
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(Integer id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public Integer getId_comentario() {
+        return id_comentario;
+    }
+
+    public void setId_comentario(Integer id_comentario) {
+        this.id_comentario = id_comentario;
+    }
+    
+    
+    
+}
